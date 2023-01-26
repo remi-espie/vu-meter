a3ba62170a5676301a9519fd35c4e319f2905673
223083658 <remi.espie@ge.com>
2023-01-26 16:58:25 +0100

    add spectrometer

--- signal_graph_maker.py
+++ signal_graph_maker.py
@@ -7,11 +7,11 @@ from matplotlib import animation
 p = pyaudio.PyAudio()
 
 # Open input stream
-stream = p.open(format=pyaudio.paInt16, channels=2, rate=44100, input=True, input_device_index=1)
+stream = p.open(format=pyaudio.paInt16, channels=2, rate=44100, input=True, input_device_index=2)
 
 # Set up the plot
 fig, ax = plt.subplots()
-ax.set_xlim(0, 44100)
+ax.set_xlim(0, 2048)
 ax.set_ylim(-32768, 32767)
 line, = ax.plot([], [])
 
@@ -20,14 +20,14 @@ stream.start_stream()
 
 
 # Plot the data
-def animate():
+def animate(i):
     data = stream.read(1024)
     data = np.frombuffer(data, dtype=np.int16)
     line.set_data(np.arange(len(data)), data)
     return line,
 
 
-ani = animation.FuncAnimation(fig, animate, frames=np.arange(0, 44100, 1024), interval=10)
+ani = animation.FuncAnimation(fig, animate, frames=np.arange(0, 44100, 1024), interval=5, blit=True)
 plt.show()
 
 # Close the stream
